b86bd5e0b8694885c14d4011cba88e72ffab2d50
fatihroy <afroyhan1@gmail.com>
2022-01-06 17:39:33 +0700

    latihan else if

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 no_angkot = 1
 jumlah_angkot = 10
 angkotYangBeroperasi = 6
-angkotYangLembur = 8
+angkotYangLembur1 = 8
+angkotYangLembur2 = 10
+angkotYangLembur3 = 5
 
 for (index = no_angkot; index <= jumlah_angkot; index++) {
 
-   if (index <= angkotYangBeroperasi) {
+    if (index === angkotYangLembur1 || index === angkotYangLembur2 || index === angkotYangLembur3 ) {
+        console.log("Angkot No. ", index, " sedang lembur")
+    
+    }
+   else if (index <= angkotYangBeroperasi) {
        console.log("Angkot No. ", index, " sedang beroperasi dengan baik")
    } 
-    else if (index === angkotYangLembur) {
-        console.log("Angkot No. ", index, " sedang lembur")
-    }
+  
     else{
         console.log("Angkot No. ", index, " sedang tydak beroperasi")
     }
