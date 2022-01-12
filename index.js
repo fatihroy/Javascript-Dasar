3d9bc6f9428ffdca9e14ad8e07f6f3733b254de2
fatihroy <afroyhan1@gmail.com>
2022-01-12 11:06:29 +0700

    latihan object

--- index.js
+++ index.js
@@ -1,60 +1,32 @@
-// program pengelolaan angkot 
+// latihan objek 
 
- // bikin array penumpang
-penumpang = []
-var tambahpenumpang = function (nama_penumpang, penumpang) {
-    // jika kursi kosong tambah penumpang
-    if (penumpang.length == 0) {
-        penumpang.push(nama_penumpang)
-        return penumpang
+function Angkot(nama_sopir, penumpang, trayek, kas) {
+    this.nama_sopir = nama_sopir,
+    this.penumpang = penumpang,
+    this.trayek = trayek,
+    this.kas = kas
+
+    // bikin fungsi penumpang naik
+    this.penumpangnaik = function (nama_penumpang) {
+       this.penumpang.push(nama_penumpang)
+        return this.penumpang
     }
-
-    // jika kursi tidak kosong
-    else{
-    // penelusuran penumpang 
-   for (var i = 0; i <= penumpang.length; i++) {
-
-    // jika kusinya kosong
-       if (penumpang[i] === undefined) {
-        //  tambah penumpang dikursi tersebut
-           penumpang[i] = nama_penumpang
-        //    kembalikan array
-        return penumpang
-       }
-       else if (penumpang[i] == nama_penumpang) {
-        //    pesan kesalahan
-        console.log(nama_penumpang+ ' sudah ada didalam angkot')
-        // kembalikan nilai
-        return penumpang
-       }
-    //   jika seluruh kursi terisi
-    else if(i == penumpang.length-1){
-        penumpang.push(nama_penumpang)
-        return penumpang
-    }
-   }
-}
-}
-
-var hapuspenumpang = function (nama_penumpang, penumpang) {
-    // jika angkot kosong
-    if (penumpang.length == 0) {
-        console.log('tidak ada penumpang didalam angkot')
-        return penumpang
-    }
-    else{
-        for ( index = 0; index < penumpang.length; index++) {
-            if (nama_penumpang == penumpang[index]) {
-                console.log(nama_penumpang, ' turun dari angkot')
-                penumpang[index] = undefined
+    this.penumpangturun = function (nama_penumpang, bayar) {
+        for ( index = 0; index < this.penumpang.length; index++) {
+           if (this.penumpang[index] == nama_penumpang) {
+              this.penumpang[index] = undefined
+              this.kas += bayar
+              return penumpang
+           }
+            else if (index == this.penumpang.length - 1) {
+                console.log(nama_penumpang + ' turun dari angkot')
                 return penumpang
             }
-          else if (index == penumpang.length - 1) {
-            console.log(nama_penumpang, ' tidak ada di angkot')
-            return penumpang
-          }
+            
         }
     }
 }
-    
+
+// bikin angkot
+var angkot1 = new Angkot ('dedy', [] , 'krian-taman', 0)
 
